6e25d2fc5be9c63d10b2f02b568d652412a26627
RomaLetodiani <roman.letodiani03@gmail.com>
2023-12-13 05:22:14 +0400

    finish burger

--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -14,13 +14,15 @@ const Burger = ({ img, name, onClick, selectedBurger }: Props) => {
   return (
     <div
       onClick={onClick}
-      className={`cursor-pointer p-1 relative max-w-[120px] flex justify-center items-center rounded-xl transition-all border ${
+      className={`cursor-pointer p-1 m-2 relative w-[120px] h-[120px] flex justify-center items-center rounded-xl transition-all border ${
         selectedBurger.name === name && 'border-[#ff944c] mx-5 scale-125'
       }`}
     >
       <span
         className={`absolute ${
-          selectedBurger.name === name ? 'md:opacity-100' : 'opacity-0'
+          selectedBurger.name === name
+            ? 'min-[1000px]:opacity-100'
+            : 'opacity-0'
         } transition-all text-[#ff944c] opacity-0 text-3xl -top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2`}
       >
         <FaSortDown />
