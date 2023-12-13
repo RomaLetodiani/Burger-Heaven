2f1582bae54a15bc5a77d1f168d1c41de0057ec2
RomaLetodiani <roman.letodiani03@gmail.com>
2023-12-13 04:08:33 +0400

    finish head

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,18 +3,23 @@ import { FaFacebookSquare, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import { headerLis } from './shared/const';
 const Header = () => {
   return (
-    <header>
+    <header className="max-w-7xl mx-auto text-white py-5 flex justify-between items-center">
       <div>
-        <img src={logo} alt="Burger Haven" />
+        <img className="w-24 invert" src={logo} alt="Burger Haven" />
       </div>
       <nav>
-        <ul>
+        <ul className="flex text-xl font-bold gap-12">
           {headerLis.map((item) => (
-            <li key={item.id}>{item.name}</li>
+            <li
+              className={`${item.id === 1 && 'text-[#ef6f1a]'} cursor-pointer`}
+              key={item.id}
+            >
+              {item.name}
+            </li>
           ))}
         </ul>
       </nav>
-      <div>
+      <div className="flex text-xl gap-8">
         <FaFacebookSquare />
         <FaLinkedin />
         <FaInstagram />
